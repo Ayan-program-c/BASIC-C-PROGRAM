0e82074915bda5c0961eb4726710b784b7e9bcd4
Ayan-program-c <asmrprog009@gmail.com>
2024-09-13 12:59:49 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    printf("\n");
+    printf("-----------Info-----------\n\n");
+    
+    char name[10];
+    printf("Enter your name : ");
+    scanf("%s", name);
+
+    printf("\n\n");
+
+    int main;
+    printf("Your Information :- \n \n");
+    printf("Name : %s", name);
+
+    printf("\n\n");
+
+    char any[10];
+    printf("Press A AND ENTER to continue....");
+    scanf("%s", &any);
+    
+    printf("\n");
+
+    return 0;
+}
